74689b82949c2a289b49a9ff28273478af56d389
Victor Paoli <victor.paoli.louvel@gmail.com>
2025-08-18 18:41:56 +0200

    core: setup settings, configs and packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
-  expoConfig,
+  expoConfig, // Importe les règles recommandées pour Expo
   {
-    ignores: ['dist/*'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // Ajoute Prettier comme "règle ESLint"
+      'prettier/prettier': 'error',
+    },
+    ignores: ['dist/*'], // Ignore le dossier dist (builds)
   },
 ]);
